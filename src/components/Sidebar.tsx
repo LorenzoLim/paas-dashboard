c5d7a00939c7b468c54eb10a85282f409a3a1909
Lorenzo Lim <renzo-lim@hotmail.com>
2025-06-19 17:35:08 +1000

    Added box shadow and logo to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,12 @@
 export const Sidebar = () => {
-  return <div className="absolute w-60 h-full bg-gray-400">Test</div>;
+  return (
+    <div className="absolute w-60 h-full shadow-xl bg-gray-300) p-10">
+      <div className="mt-17 flex justify-between">
+        <div>
+          <img src="./vite.svg" style={{ width: 50, height: 50 }} />
+        </div>
+        <span className="p-1 bold text-primary">Vite Dashboard</span>
+      </div>
+    </div>
+  );
 };
