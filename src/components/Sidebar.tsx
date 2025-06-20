d48b49e6d184d0280e1be919363799c92a150483
Lorenzo Lim <renzo-lim@hotmail.com>
2025-06-20 22:31:52 +1000

    Added Graphs Navigation Option

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,25 @@
+import { ChartBar, ChevronRight } from "lucide-react";
+
 export const Sidebar = () => {
   return (
-    <div className="absolute w-60 h-full shadow-xl bg-gray-300) p-10">
-      <div className="mt-17 flex justify-between">
+    <div className="absolute w-60 h-full shadow-xl bg-gray-300) p-5 mt-17">
+      <div className="flex justify-between">
         <div>
           <img src="./vite.svg" style={{ width: 50, height: 50 }} />
         </div>
         <span className="p-1 bold text-primary">Vite Dashboard</span>
+      </div>
+      <div className="pt-10">
+        <div className="text-primar text-left">General</div>
+        <div className="p-3 flex justify-between">
+          <div className="flex">
+            <ChartBar className="text-primary" />{" "}
+            <span className="pl-2">Graphs</span>
+          </div>
+          <div>
+            <ChevronRight className="text-primary" />
+          </div>
+        </div>
       </div>
     </div>
   );
