c0adbfff87694d338b400d9ef7668495f838823e
Lorenzo Lim <renzo-lim@hotmail.com>
2025-06-22 12:52:52 +1000

    Added hover animation to sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Home } from "./pages/Homes";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,35 +1,108 @@
-import { ChartBar, ChevronRight, MapPin } from "lucide-react";
+import {
+  Activity,
+  Bug,
+  ChartBar,
+  ChevronRight,
+  Component,
+  FileText,
+  MapPin,
+  PaintRoller,
+  Wrench,
+} from "lucide-react";
 
 export const Sidebar = () => {
   return (
-    <div className="absolute w-60 h-full shadow-xl bg-gray-300) p-5 mt-17">
-      <div className="flex justify-between">
-        <div>
-          <img src="./vite.svg" style={{ width: 50, height: 50 }} />
-        </div>
-        <span className="p-1 bold text-primary">Vite Dashboard</span>
-      </div>
-      <div className="pt-10">
-        <div className="text-primar text-left">General</div>
-        <div className="p-3 flex justify-between">
-          <div className="flex">
-            <ChartBar className="text-primary" />
-            <span className="pl-2">Graphs</span>
-          </div>
+    <div className="absolute w-60 h-[calc(100vh-68px)] shadow-xl bg-gray-300) p-5 mt-17 flex flex-col justify-between">
+      <div>
+        <div className="flex">
           <div>
-            <ChevronRight className="text-primary" />
+            <img src="./vite.svg" style={{ width: 60, height: 60 }} />
+          </div>
+          <span className="p-1 bold text-primary text-lg">
+            PaaS Dashboard{" "}
+            <span className="text-sm text-black">powered by: Vite</span>
+          </span>
+        </div>
+        <div className="pt-10">
+          <div className=" text-left">General</div>
+          <div className="group p-3 flex justify-between hover:cursor-pointer hover:bg-gray-200">
+            <div className="flex">
+              <ChartBar className="text-primary" />
+              <span className="pl-2 text-primary">Graphs</span>
+            </div>
+            <div>
+              <ChevronRight className="text-primary group-hover:translate-x-2 transition-all duration-300" />
+            </div>
+          </div>
+          <div className="group p-3 flex justify-between hover:cursor-pointer hover:bg-gray-200">
+            <div className="flex">
+              <MapPin className="text-primary" />
+              <span className="pl-2 text-primary">Maps</span>
+            </div>
+            <div>
+              <ChevronRight className="text-primary group-hover:translate-x-2 transition-all duration-300" />
+            </div>
+          </div>
+          <div className="group p-3 flex justify-between hover:cursor-pointer hover:bg-gray-200">
+            <div className="flex">
+              <Activity className="text-primary" />
+              <span className="pl-2 text-primary">Activity</span>
+            </div>
+            <div>
+              <ChevronRight className="text-primary group-hover:translate-x-2 transition-all duration-300" />
+            </div>
+          </div>
+          <div className="group p-3 flex justify-between hover:cursor-pointer hover:bg-gray-200">
+            <div className="flex">
+              <Component className="text-primary" />
+              <span className="pl-2 text-primary">Components</span>
+            </div>
+            <div>
+              <ChevronRight className="text-primary group-hover:translate-x-2 transition-all duration-300" />
+            </div>
+          </div>
+          <div className="group p-3 flex justify-between hover:cursor-pointer hover:bg-gray-200">
+            <div className="flex">
+              <FileText className="text-primary" />
+              <span className="pl-2 text-primary">Documentation</span>
+            </div>
+            <div>
+              <ChevronRight className="text-primary group-hover:translate-x-2 transition-all duration-300" />
+            </div>
           </div>
         </div>
-        <div className="p-3 flex justify-between">
-          <div className="flex">
-            <MapPin className="text-primary" />
-            <span className="pl-2">Maps</span>
+        <div className="pt-10">
+          <div className="text-primar text-left">Tools</div>
+          <div className="group p-3 flex justify-between hover:cursor-pointer hover:bg-gray-200">
+            <div className="flex">
+              <Wrench className="text-primary" />
+              <span className="pl-2 text-primary">Settings</span>
+            </div>
+            <div>
+              <ChevronRight className="text-primary group-hover:translate-x-2 transition-all duration-300" />
+            </div>
           </div>
-          <div>
-            <ChevronRight className="text-primary" />
+          <div className="group p-3 flex justify-between hover:cursor-pointer hover:bg-gray-200">
+            <div className="flex">
+              <PaintRoller className="text-primary" />
+              <span className="pl-2 text-primary">Theme</span>
+            </div>
+            <div>
+              <ChevronRight className="text-primary group-hover:translate-x-2 transition-all duration-300" />
+            </div>
+          </div>
+          <div className="group p-3 flex justify-between hover:cursor-pointer hover:bg-gray-200">
+            <div className="flex">
+              <Bug className="text-primary" />
+              <span className="pl-2 text-primary">Debug</span>
+            </div>
+            <div>
+              <ChevronRight className="text-primary group-hover:translate-x-2 transition-all duration-300" />
+            </div>
           </div>
         </div>
       </div>
+      <div className="text-primary">Vite PaaS dashboard by Lorenzo Lim</div>
     </div>
   );
 };
