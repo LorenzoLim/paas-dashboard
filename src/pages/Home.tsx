59c6dfc094b706f00d4df5b6b9b6fe0d9098bb7c
Lorenzo Lim <renzo-lim@hotmail.com>
2025-06-24 19:13:12 +1000

    Added content to table

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -83,12 +83,163 @@ export const Home = () => {
           </div>
           <div className="bg-card p-5 mt-10 rounded shadow-xs h-200">
             <div className="flex justify-between border-b-2 border-black-900 pb-5">
-              <h2 className="text-xl font-bold tracking-tight">
-                Product Summary
-              </h2>
+              <h2 className="text-xl font-bold tracking-tight">Users</h2>
               <div>
                 <button className="cosmic-button cursor-pointer">Export</button>
               </div>
+            </div>
+            <div className="relative flex flex-col w-full h-full mt-10 overflow-scroll text-gray-700 bg-white shadow-md rounded-xl bg-clip-border">
+              <table className="w-full text-left table-auto min-w-max">
+                <thead>
+                  <tr>
+                    <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                      <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                        Name
+                      </p>
+                    </th>
+                    <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                      <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                        Job
+                      </p>
+                    </th>
+                    <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                      <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                        Employed
+                      </p>
+                    </th>
+                    <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                      <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70"></p>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className="p-4 border-b border-blue-gray-50">
+                      <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        John Michael
+                      </p>
+                    </td>
+                    <td className="p-4 border-b border-blue-gray-50">
+                      <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        Manager
+                      </p>
+                    </td>
+                    <td className="p-4 border-b border-blue-gray-50">
+                      <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        23/04/18
+                      </p>
+                    </td>
+                    <td className="p-4 border-b border-blue-gray-50">
+                      <a
+                        href="#"
+                        className="block font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900"
+                      >
+                        Edit
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="p-4 border-b border-blue-gray-50">
+                      <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        Alexa Liras
+                      </p>
+                    </td>
+                    <td className="p-4 border-b border-blue-gray-50">
+                      <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        Developer
+                      </p>
+                    </td>
+                    <td className="p-4 border-b border-blue-gray-50">
+                      <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        23/04/18
+                      </p>
+                    </td>
+                    <td className="p-4 border-b border-blue-gray-50">
+                      <a
+                        href="#"
+                        className="block font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900"
+                      >
+                        Edit
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="p-4 border-b border-blue-gray-50">
+                      <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        Laurent Perrier
+                      </p>
+                    </td>
+                    <td className="p-4 border-b border-blue-gray-50">
+                      <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        Executive
+                      </p>
+                    </td>
+                    <td className="p-4 border-b border-blue-gray-50">
+                      <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        19/09/17
+                      </p>
+                    </td>
+                    <td className="p-4 border-b border-blue-gray-50">
+                      <a
+                        href="#"
+                        className="block font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900"
+                      >
+                        Edit
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="p-4 border-b border-blue-gray-50">
+                      <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        Michael Levi
+                      </p>
+                    </td>
+                    <td className="p-4 border-b border-blue-gray-50">
+                      <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        Developer
+                      </p>
+                    </td>
+                    <td className="p-4 border-b border-blue-gray-50">
+                      <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        24/12/08
+                      </p>
+                    </td>
+                    <td className="p-4 border-b border-blue-gray-50">
+                      <a
+                        href="#"
+                        className="block font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900"
+                      >
+                        Edit
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="p-4">
+                      <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        Richard Gran
+                      </p>
+                    </td>
+                    <td className="p-4">
+                      <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        Manager
+                      </p>
+                    </td>
+                    <td className="p-4">
+                      <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        04/10/21
+                      </p>
+                    </td>
+                    <td className="p-4">
+                      <a
+                        href="#"
+                        className="block font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900"
+                      >
+                        Edit
+                      </a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
